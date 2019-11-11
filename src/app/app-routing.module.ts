4a2ba9c78a1ce3be823b331766df59216a61e240
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2019-11-11 09:45:07 +0530

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BooksComponent } from './compornent/books/books.component';
+import { SignInComponent } from './compornent/sign-in/sign-in.component';
+import { SignUpComponent } from './compornent/sign-up/sign-up.component';
+import { DashboardComponent } from './compornent/dashboard/dashboard.component';
+import { AddBookComponent } from './compornent/add-book/add-book.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',redirectTo: '/books', pathMatch: 'full'},
+  {path:'books', component: BooksComponent},
+  {path: 'add-book', component: AddBookComponent},
+  {path: 'sign-in', component : SignInComponent},
+  {path: 'sign-up', component : SignUpComponent},
+  {path : 'dashbord', component: DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,20 @@ import { BooksComponent } from './compornent/books/books.component';
 import { BookService } from './services/book.service';
 import { AddBookComponent } from './compornent/add-book/add-book.component';
 import {FormsModule } from '@angular/forms';
+import { HeaderComponent } from './compornent/header/header.component';
+import { SignInComponent } from './compornent/sign-in/sign-in.component';
+import { SignUpComponent } from './compornent/sign-up/sign-up.component';
+import { DashboardComponent } from './compornent/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BooksComponent,
     AddBookComponent,
+    HeaderComponent,
+    SignInComponent,
+    SignUpComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
